3945757b4ec0c549e0bc2fd52766b475f2191765
Mariyana-Katz <mariqnak21@gmail.com>
2024-03-08 22:27:55 -0500

    finished

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -1,6 +1,8 @@
 package io.zipcoder;
 
 
+import java.util.ArrayList;
+
 /**
  * @author tariq
  */
@@ -14,8 +16,16 @@ public class StringsAndThings {
      *           countYZ("day fez"); // Should return 2
      *           countYZ("day fyyyz"); // Should return 2
      */
+
     public Integer countYZ(String input){
-        return null;
+        int count = 0;
+        String [] array = input.split(" ");//separate the words with space.
+        for(String word: array){
+            if (word.charAt(word.length() -1) == 'z' || word.charAt(word.length() -1) == 'y'){//going trougth each word and checking for z or y in the end
+                count++;//count them
+            }
+        }
+        return count;
     }
 
     /**
@@ -28,7 +38,10 @@ public class StringsAndThings {
      *           removeString("Hello there", "x") // Should return "Hello there"
      */
     public String removeString(String base, String remove){
-        return null;
+       //I made a new string named it base1 and then I call the replace method in the () I put remove(the second par and then replaced it with empty string.
+        String base1 = base.replace(remove, "");
+
+        return base1;
     }
 
     /**
@@ -39,8 +52,27 @@ public class StringsAndThings {
      *           containsEqualNumberOfIsAndNot("This is notnot") // Should return true
      *           containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
      */
-    public Boolean containsEqualNumberOfIsAndNot(String input){
-        return null;
+    public Boolean containsEqualNumberOfIsAndNot(String input) {
+        //Declaring two counters.
+        int isCounter = 0;
+        int notCounter = 0;
+        //making a for loop to run trought the string.Then checking for 'i' and 's'(using i+1 because is next to the 'i'.
+        for (int i = 0; i < input.length() - 1; i++) {
+            if (input.charAt(i) == 'i' && input.charAt(i + 1) == 's') {
+                isCounter++;
+            }
+        }
+        //Then another for loop for checking for char 'n', 'o', 't'. And here the length is 22, otherwise will not be able to finish the "not"
+        for (int i = 0; i < input.length() - 2; i++) {
+            if (input.charAt(i) == 'n' && input.charAt(i + 1) == 'o' && input.charAt(i + 2) == 't') {
+                notCounter++;
+            }
+        }
+            //Checking if both counters are equal.
+        if (isCounter == notCounter)
+            return true;
+         else
+            return false;
     }
 
     /**
@@ -50,8 +82,15 @@ public class StringsAndThings {
      *           gHappy("xxgxx") // Should return  false
      *           gHappy("xxggyygxx") // Should return  false
      */
-    public Boolean gIsHappy(String input){
-        return null;
+   public Boolean gIsHappy(String input){
+        for( int i = 0; i< input.length(); i++){
+            if(input.charAt(i) == 'g'){
+                if(input.charAt(i-1) =='g' || input.charAt(i+1)=='g'){
+                    return true;}
+
+                }
+            }return false;
+
     }
 
 
@@ -62,7 +101,20 @@ public class StringsAndThings {
      *            countTriple("xxxabyyyycd") // Should return 3
      *            countTriple("a") // Should return 0
      */
-    public Integer countTriple(String input){
-        return null;
+   public Integer countTriple(String input) {
+       //declaring counter and char(x)
+        int counter = 0;
+        char x;
+       //making a for loop to run trougth the loop.
+        for(int i = 0; i<input.length() -2; i++){
+           //assigning a current char in a string.
+            x = input.charAt(i);
+          // Using a if statement to check for three equal char.
+            if ( input.charAt(i + 1) == x && input.charAt(i + 2) == x) {
+               counter ++;
+            }
+
+        }
+        return counter;
     }
 }
